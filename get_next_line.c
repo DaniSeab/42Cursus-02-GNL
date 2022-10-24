549fea1cabd5eb0f40122f5869daa163afa0c476
Dani Seabra <103091767+DaniSeab@users.noreply.github.com>
2022-10-23 21:46:26 -0300

    OK + commented [23/10/22]

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-//joins 2 strings and frees the first
+//joins 2 strings and frees the first, to allow us to return value to the same pointer
 char	*ft_strjoin_gnl(char *s1, char *s2)
 {
 	char	*res;
@@ -35,12 +35,13 @@ char	*ft_strjoin_gnl(char *s1, char *s2)
 		i++;
 		j++;
 	}
+	//s1 is the same pointer as str_read, so we free it so we can change its value to the returned value
 	free(s1);
 	s1 = NULL;
 	return (res);
 }
 
-//duplicates until nl
+//duplicates input string until \n is found
 char	*ft_strdup_gnl_nl(char *src)
 {
 	char	*dest;
@@ -63,7 +64,7 @@ char	*ft_strdup_gnl_nl(char *src)
 	return (dest);
 }
 
-//reads fd and joins result into static char *
+//reads fd and joins read results into static char str_read
 char	*read_file(int fd, char *str_read)
 {
 	int		bytes_read;
@@ -75,6 +76,7 @@ char	*read_file(int fd, char *str_read)
 	if (!temp || !str_read)
 		return (NULL);
 	bytes_read = 1;
+	//when theres nothing else to read, bytes_read will be 0
 	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, temp, BUFFER_SIZE);
@@ -84,8 +86,11 @@ char	*read_file(int fd, char *str_read)
 			temp = NULL;
 			return (NULL);
 		}
+		//set byte after last one read to \0 to close string
 		temp[bytes_read] = '\0';
+		//joins string just read with value held on static char, which was copied into str_read
 		str_read = ft_strjoin_gnl(str_read, temp);
+		//stop reading when it finds a \n
 		if (ft_strchr(temp, '\n'))
 			break ;
 	}
@@ -93,23 +98,31 @@ char	*read_file(int fd, char *str_read)
 	return (str_read);
 }
 
+//each time its called it returns a new line from file of input fd
 char	*get_next_line(int fd)
 {
 	char		*res;
 	static char	*st_str;
 
+	//tests if inputs are valid
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
+	//reads the file and sets static variable st_str to its return
 	st_str = read_file(fd, st_str);
 	if (!st_str)
 		return (NULL);
+	//output string will be read string up until \n
 	res = ft_strdup_gnl_nl(st_str);
+	//if theres an \n in st_str and theres something other than \0 after it
 	if (ft_strchr(st_str, '\n') && *(ft_strchr(st_str, '\n') + 1) != '\0')
+		//substitute the value in st_str for the value after \n
 		st_str = ft_strdup(st_str, ft_strchr(st_str, '\n') + 1);
 	else
 	{
+		//if there's nothing to hold in st_str, free st_str and set it to NULL to avoid dangling pointer
 		free(st_str);
 		st_str = NULL;
 	}
+	//returns read string up until \n, and holds in st_str everything after it for the next call
 	return (res);
 }
